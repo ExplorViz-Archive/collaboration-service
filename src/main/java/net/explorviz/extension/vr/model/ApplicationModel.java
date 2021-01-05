a1d34170b1e82dab062baf80c15d3bae61aaac4c
Johannes-Brück <brueckjo@web.de>
2021-01-05 17:49:17 +0100

    add application model and service

---
+++ src/main/java/net/explorviz/extension/vr/model/ApplicationModel.java
@@ -0,0 +1,67 @@
+package net.explorviz.extension.vr.model;
+
+import java.util.ArrayList;
+
+public class ApplicationModel extends BaseModel {
+
+    boolean isOpen;
+    boolean isGrabbed;
+    String grabbedByUser;
+
+    private final ArrayList<String> openComponents;
+
+    public ApplicationModel() {
+        openComponents = new ArrayList<>();
+    }
+
+    public boolean isOpen() {
+        return this.isOpen;
+    }
+
+    public void setOpen(final boolean isOpen) {
+        this.isOpen = isOpen;
+    }
+
+    public void openComponent(final String id) {
+        openComponents.add(id);
+    }
+
+    public void closeComponent(final String id) {
+        openComponents.remove(id);
+    }
+
+    public void closeAllComponents() {
+        openComponents.clear();
+    }
+
+    public ArrayList<String> getOpenComponents() {
+        return openComponents;
+    }
+
+    public void setGrabbed(final boolean isGrabbed) {
+        this.isGrabbed = isGrabbed;
+    }
+
+    public boolean isGrabbed() {
+        return isGrabbed;
+    }
+
+    public void setGrabbedByUser(final String userID) {
+        this.grabbedByUser = userID;
+    }
+
+    public void setUnboundByUser(final String userID) {
+        if (this.isGrabbedByUser(userID)) {
+            this.setGrabbed(false);
+        }
+    }
+
+    public boolean isGrabbedByUser(final String userID) {
+        if (this.isGrabbed && userID == this.grabbedByUser) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ src/main/java/net/explorviz/extension/vr/model/BaseModel.java
@@ -0,0 +1,71 @@
+package net.explorviz.extension.vr.model;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class BaseModel {
+
+    private static final AtomicLong ID_GENERATOR = new AtomicLong();
+
+    // position data
+    private double xPos = 0;
+    private double yPos = 0;
+    private double zPos = 0;
+    private double xQuat, yQuat, zQuat, wQuat;
+
+    private String id;
+
+    public BaseModel() {
+        id = String.valueOf(ID_GENERATOR.incrementAndGet());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(final String id) {
+        this.id = id;
+    }
+
+    public double[] getPosition() {
+        final double[] coordinates = { xPos, yPos, zPos };
+        return coordinates;
+    }
+
+    public void setPosition(final double[] coordinates) {
+        this.xPos = coordinates[0];
+        this.yPos = coordinates[1];
+        this.zPos = coordinates[2];
+    }
+
+    public void setDeltaPosition(final double[] coordinates) {
+        this.xPos += coordinates[0];
+        this.yPos += coordinates[1];
+        this.zPos += coordinates[2];
+    }
+
+    public void setPosition(final double x, final double y, final double z) {
+        this.xPos = x;
+        this.yPos = y;
+        this.zPos = z;
+    }
+
+    public double[] getQuaternion() {
+        final double[] quaternion = { xQuat, yQuat, zQuat, wQuat };
+        return quaternion;
+    }
+
+    public void setQuaternion(final double[] quaternion) {
+        this.xQuat = quaternion[0];
+        this.yQuat = quaternion[1];
+        this.zQuat = quaternion[2];
+        this.wQuat = quaternion[3];
+    }
+
+    public void setQuaternion(final double xQuat, final double yQuat, final double zQuat, final double wQuat) {
+        this.xQuat = xQuat;
+        this.yQuat = yQuat;
+        this.zQuat = zQuat;
+        this.wQuat = wQuat;
+    }
+
+}
