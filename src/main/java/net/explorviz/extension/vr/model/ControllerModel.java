cb94a067f7e09d690230aace6e04192e37ad1483
Johannes Brück <stu204865@uni-kiel.de>
2021-01-06 13:00:03 +0100

    add model for user, controller, and highlighting

---
+++ src/main/java/net/explorviz/extension/vr/model/ControllerModel.java
@@ -0,0 +1,18 @@
+package net.explorviz.extension.vr.model;
+
+public class ControllerModel extends BaseModel {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public ControllerModel() {
+	}
+
+}
