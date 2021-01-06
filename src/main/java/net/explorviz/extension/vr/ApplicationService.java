ce6028cc2feb78c0442a1da90bbf35ff5df17f67
Johannes Brück <stu204865@uni-kiel.de>
2021-01-06 11:27:58 +0100

    add missing methods to application service

--- src/main/java/net/explorviz/extension/vr/ApplicationService.java
+++ src/main/java/net/explorviz/extension/vr/ApplicationService.java
@@ -2,11 +2,16 @@ package net.explorviz.extension.vr;
 
 import java.util.HashMap;
 
+import javax.enterprise.context.ApplicationScoped;
+
 import net.explorviz.extension.vr.messages.AppClosedMessage;
 import net.explorviz.extension.vr.messages.AppGrabbedMessage;
 import net.explorviz.extension.vr.messages.AppOpenedMessage;
+import net.explorviz.extension.vr.messages.AppReleasedMessage;
+import net.explorviz.extension.vr.messages.AppTranslatedMessage;
 import net.explorviz.extension.vr.model.ApplicationModel;
 
+@ApplicationScoped
 public class ApplicationService {
 
     private final HashMap<String, ApplicationModel> apps = new HashMap<>(); // maps applicationID to the application
@@ -44,6 +49,28 @@ public class ApplicationService {
         return true;
     }
 
-    // TODO onAppReleased, onAppTranslated
+    public void onAppReleased(final AppReleasedMessage message) {
+        ApplicationModel appModel;
+        String id = message.getId();
+
+        // add app to hash map or get app from hash map
+        if (this.apps.containsKey(id)) {
+            appModel = this.apps.get(id);
+        } else {
+            appModel = new ApplicationModel();
+            appModel.setId(id);
+            this.apps.put(id, appModel);
+        }
+
+        appModel.setOpen(true);
+        appModel.setPosition(message.getPosition());
+        appModel.setQuaternion(message.getQuaternion());
+        appModel.setGrabbed(false);
+    }
+    
+    public void onAppTranslated(final AppTranslatedMessage message) {
+    	
+    }
+
 
 }
