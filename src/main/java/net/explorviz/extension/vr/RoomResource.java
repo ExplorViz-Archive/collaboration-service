400bb36227a713d160ed3a10ce13bd56b30102fd
Johannes-Brück <brueckjo@web.de>
2021-03-18 13:22:00 +0100

    add room resource

---
+++ src/main/java/net/explorviz/extension/vr/RoomResource.java
@@ -0,0 +1,34 @@
+package net.explorviz.extension.vr;
+
+import java.util.Set;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import net.explorviz.extension.vr.service.RoomService;
+
+@Path("/v2/vr")
+public class RoomResource {
+    
+    @Inject
+    RoomService roomService;
+
+    @GET
+    @Path("/rooms")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Set<String> list() {
+        return roomService.getRooms();
+    }
+    
+    @POST
+    @Path("/room")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String add() {
+        return roomService.createRoom();
+    }
+    
+}
