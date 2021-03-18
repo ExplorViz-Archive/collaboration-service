bf1f0781d99f9a9e53c0f942234c5925e6892a1f
Johannes-Brück <brueckjo@web.de>
2021-03-18 12:17:41 +0100

    add room service

--- src/main/java/net/explorviz/extension/vr/service/RoomService.java
+++ src/main/java/net/explorviz/extension/vr/service/RoomService.java
@@ -1,5 +1,9 @@
 package net.explorviz.extension.vr.service;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
@@ -10,4 +14,27 @@ public class RoomService {
 
     @Inject
     RoomFactory roomFactory;
+    
+    @Inject
+    IdGenerationService idGenerationService;
+    
+    private Map<String, Room> rooms = new ConcurrentHashMap<>();
+    
+    public Room createRoom() {
+        var room = roomFactory.makeRoom();
+        rooms.put(idGenerationService.nextId(), room);
+        return room;
+    }
+    
+    public void deleteRoom(String roomId) {
+        rooms.remove(roomId);
+    }
+    
+    public Room lookupRoom(String roomId) {
+        return rooms.get(roomId);
+    }
+    
+    public Collection<Room> getRooms() {
+        return rooms.values();
+    }
 }
