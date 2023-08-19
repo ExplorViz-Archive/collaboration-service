71d8cc11e633b3fc2e8d1a03791c0c229b5a41d1
stu108573@mail.uni-kiel.de <stu108573@mail.uni-kiel.de>
2023-08-19 10:50:05 +0200

    Remove redundant variable

--- src/main/java/net/explorviz/collaboration/payload/receivable/InitialSynchronizationPayload.java
+++ src/main/java/net/explorviz/collaboration/payload/receivable/InitialSynchronizationPayload.java
@@ -1,22 +1,14 @@
 package net.explorviz.collaboration.payload.receivable;
 
 /**
- * A JSON object that is sent by the frontend to the room resource when a new room is created.
+ * A JSON object that is sent by the frontend to the room resource when a new
+ * room is created.
  * Contains all information to initialize a room.
  */
 public class InitialSynchronizationPayload {
 
-    private Boolean needRoom;
     private InitialRoomPayload roomPayload;
     private JoinLobbyPayload joinPayload;
-    
-    public Boolean getNeedRoom() {
-        return this.needRoom;
-    }
-
-    public void setNeedRoom(Boolean needRoom) {
-        this.needRoom = needRoom;
-    }
 
     public InitialRoomPayload getRoomPayload() {
         return this.roomPayload;
