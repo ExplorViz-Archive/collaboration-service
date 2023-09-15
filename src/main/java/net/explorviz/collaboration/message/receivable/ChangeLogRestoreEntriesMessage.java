be6614a7ea03e8707efabf94a7099ad9b6fe065a
Arash Giv <stu209864@mail.uni-kiel.de>
2023-09-16 01:02:06 +0200

    update collab for sync changelog

--- src/main/java/net/explorviz/collaboration/message/receivable/ChangeLogRestoreEntriesMessage.java
+++ src/main/java/net/explorviz/collaboration/message/receivable/ChangeLogRestoreEntriesMessage.java
@@ -6,8 +6,18 @@ import net.explorviz.collaboration.message.ReceivableMessageHandler;
 public class ChangeLogRestoreEntriesMessage extends ReceivableMessage {
   public static final String EVENT = "changelog_restore_entries";
 
+  private String key;
+
   public ChangeLogRestoreEntriesMessage() {
     super(EVENT);
+  }
+  
+  public String getKey() {
+    return this.key;
+  }
+
+  public void setKey(final String key) {
+    this.key = key;
   }
 
   @Override
